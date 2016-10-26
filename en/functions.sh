7f5436bda8c085e527d4f489e2651a7b704d80a6
Alexandre Mély <alexandre.mely@gmail.com>
2016-10-26 11:43:37 +0200

    added en language support

---
+++ en/functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+jarvis_gmail () {
+    local nb=$(curl -u $gmail_username:$gmail_password --silent "https://mail.google.com/mail/feed/atom" | sed -n "s/.*<fullcount>\(.*\)<\/fullcount>.*/\1/p")
+    if [ $nb -ne 0 ]; then
+        echo "You have $nb unread emails"
+    else
+        echo "You have no unread emails"
+    fi
+}
