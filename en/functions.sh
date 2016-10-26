deeb0cb271bd74a220e6702dabb70c713887025e
Alexandre Mély <alexandre.mely@gmail.com>
2016-10-26 11:46:18 +0200

    english support

--- en/functions.sh
+++ en/functions.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 jarvis_gmail () {
     local nb=$(curl -u $gmail_username:$gmail_password --silent "https://mail.google.com/mail/feed/atom" | sed -n "s/.*<fullcount>\(.*\)<\/fullcount>.*/\1/p")
     if [ $nb -ne 0 ]; then
